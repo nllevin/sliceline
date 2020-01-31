a34d97cb78e337131508fc91e5e2c404c15f8f6c
nllevin <50681582+nllevin@users.noreply.github.com>
2020-01-31 17:41:40 -0500

    Add fetchVenueDetails

--- src/util/venue_api_util.js
+++ src/util/venue_api_util.js
@@ -10,6 +10,15 @@ export const fetchVenues = ({ latitude, longitude }) => {
   params.append("client_secret", CLIENT_SECRET);
   params.append("ll", `${latitude},${longitude}`);
   params.append("query", "pizza");
+  params.append("limit", "1");
   params.append("v", "20200131");
   return axios.get(`https://api.foursquare.com/v2/venues/search?${params.toString()}`);
+};
+
+export const fetchVenueDetails = venueId => {
+  const params = new URLSearchParams();
+  params.append("client_id", CLIENT_ID);
+  params.append("client_secret", CLIENT_SECRET);
+  params.append("v", "20200131");
+  return axios.get(`https://api.foursquare.com/v2/venues/${venueId}?${params.toString()}`);
 };
